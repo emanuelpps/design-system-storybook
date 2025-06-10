a04874664a396fcfecb2244bf115ab5a381df3f7
emanuelpps <emanuelpages.ps@gmail.com>
2025-06-09 23:39:28 -0300

    chore: added coverage for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
-    viteConfig,
-    defineConfig({
-        test: {
-            globals: true,
-            setupFiles: './test/setup',
-            environment: 'jsdom',
-            coverage: {
-                thresholds: {
-                    lines: 50,
-                    functions: 50,
-                    branches: 50,
-                    statements: 50
-                }
-            }
-        }
-    })
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      setupFiles: "./src/testing/setup.ts",
+      environment: "jsdom",
+      coverage: {
+        thresholds: {
+          lines: 50,
+          functions: 50,
+          branches: 50,
+          statements: 50,
+        },
+      },
+    },
+  })
 );
